ebb3a129d9f5f4b76c9b605f43fe33c5f48571d4
jacobrees <jacob.rees97@yahoo.com>
2021-05-20 16:31:37 +0100

    Add mini-css-extract-plugin to production config

--- webpack(Colt-Steele)/webpack.prod.js
+++ webpack(Colt-Steele)/webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -9,5 +10,14 @@ module.exports = merge(common, {
     filename: 'main-[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [new MiniCssExtractPlugin({ filename: '[name]-[contenthash].css' }),
+    new CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
 });
