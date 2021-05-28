275d7d17713f2c71d1c5521717fffa2280bac331
jacobrees <jacob.rees97@yahoo.com>
2021-05-28 22:58:01 +0100

    Add prototype example to useful-javascript-es6

---
+++ JavaScript/useful-javascript-es6/prototype.js
@@ -0,0 +1,18 @@
+//example
+function Student() {
+}
+
+Student.prototype.sayName = function() {
+  console.log(this.name)
+}
+
+function EighthGrader(name) {
+  this.name = name
+  this.grade = 8
+}
+
+EighthGrader.prototype = Object.create(Student.prototype)
+
+const carl = new EighthGrader("carl")
+carl.sayName() // console.logs "carl"
+carl.grade // 8
